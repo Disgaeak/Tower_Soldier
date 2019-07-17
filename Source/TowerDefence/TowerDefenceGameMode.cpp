436535273dc08f3d6b16c24fd468ee60384ce7b8
sucks <briantand78@gmail.com>
2019-07-18 06:56:36 +0900

    Start

---
+++ Source/TowerDefence/TowerDefenceGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TowerDefenceGameMode.h"
+#include "TowerDefenceCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATowerDefenceGameMode::ATowerDefenceGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Mannequin/Character/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
